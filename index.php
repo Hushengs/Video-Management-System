542065d46935e2c29e9846e873c585113d3bba2c
Hushengs <1150668256@qq.com>
2015-11-13 19:12:14 +0800

    创建前台后台入口模板
    引入Thinkphp入口文件,前后台分别为Home模块和Admin模块

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//基于tp框架的影音管理系统开发
+
+//定义项目的名称
+define('APP_NAME','Video');
+
+//定义项目的路径
+define('APP_PATH','./Video/');
+
+//默认加载Home前台模块
+$_GET['m'] = 'Home';
+
+//引人ThinkPHP核心文件
+require('./ThinkPHP/ThinkPHP.php');
